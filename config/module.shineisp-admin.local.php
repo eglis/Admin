6327755b02c8c210f948d323cf711175fbf2ea2f
Michelangelo Turillo <info@shineisp.com>
2015-06-30 18:06:23 +0200

    Module improved

---
+++ config/module.shineisp-admin.local.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This is a sample "local" configuration for your application. To use it, copy
+ * it to your config/autoload/ directory of your application, and edit to suit
+ * your application.
+ */
+
+return array();
